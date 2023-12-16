4037ef9a854cdc5d2ae8bbe13e5114a33f1876b1
Rafael Jacov Medel <medelrafjac@gmail.com>
2023-12-16 10:42:56 +0800

    added favorites.html; fixed redeclaration error from share.js

--- share.js
+++ share.js
@@ -1,31 +1,34 @@
 // DISPLAYING IMAGE
-const fileInput = document.querySelector("#file");
-const validFormat = ['image/jpeg', 'image/png', 'image/webp'];
-const errorModal = document.querySelector("#modal");
+(()=>{
+    const fileInput = document.querySelector("#file");
+    const validFormat = ['image/jpeg', 'image/png', 'image/webp'];
+    const errorModal = document.querySelector("#modal");
 
-errorModal.addEventListener('click', function() {
-    this.close();
-})
+    errorModal.addEventListener('click', function() {
+        this.close();
+    })
 
-function verifyType() {
-    const file = fileInput.files[0];
-    if (validFormat.includes(file.type)) {
-        displayImage(file);
-    } else {
-        errorModal.showModal();
-    }
-}
-
-function displayImage(image) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const imgContainer = document.querySelector("#img-getter");
-        const imgNode = document.querySelector(".share img");
-
-        imgContainer.remove();
-        imgNode.classList.toggle("d-none");
-        imgNode.src = e.target.result;
+    function verifyType() {
+        const file = fileInput.files[0];
+        if (validFormat.includes(file.type)) {
+            displayImage(file);
+        } else {
+            errorModal.showModal();
+        }
     }
 
-    reader.readAsDataURL(image);
-}
+    function displayImage(image) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const imgContainer = document.querySelector("#img-getter");
+            const imgNode = document.querySelector(".share img");
+
+            imgContainer.remove();
+            imgNode.classList.toggle("d-none");
+            imgNode.src = e.target.result;
+        }
+
+        reader.readAsDataURL(image);
+    }
+
+})();
