744153a39280e4b8c2ed00476014bc8051aa97d7
Rafael Jacov Medel <medelrafjac@gmail.com>
2023-12-17 23:11:50 +0800

    moved to addEventListener

--- share.js
+++ share.js
@@ -1,5 +1,5 @@
 // DISPLAYING IMAGE
-(() => {
+(()=>{
     const fileInput = document.querySelector("#file");
     const validFormat = ['image/jpeg', 'image/png', 'image/webp'];
     const errorModal = document.querySelector("#modal");
@@ -7,27 +7,29 @@
     errorModal.addEventListener('click', function() {
         this.close();
     })
+
+    function verifyType() {
+        const file = fileInput.files[0];
+        if (validFormat.includes(file.type)) {
+            displayImage(file);
+        } else {
+            errorModal.showModal();
+        }
+    }
+
+    function displayImage(image) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const imgContainer = document.querySelector("#img-getter");
+            const imgNode = document.querySelector(".share img");
+
+            imgContainer.remove();
+            imgNode.classList.toggle("d-none");
+            imgNode.src = e.target.result;
+        }
+
+        reader.readAsDataURL(image);
+    }
+
+    fileInput.addEventListener("change", verifyType);
 })();
-
-function verifyType() {
-    const file = fileInput.files[0];
-    if (validFormat.includes(file.type)) {
-        displayImage(file);
-    } else {
-        errorModal.showModal();
-    }
-}
-
-function displayImage(image) {
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        const imgContainer = document.querySelector("#img-getter");
-        const imgNode = document.querySelector(".share img");
-
-        imgContainer.remove();
-        imgNode.classList.toggle("d-none");
-        imgNode.src = e.target.result;
-    }
-
-    reader.readAsDataURL(image);
-}
